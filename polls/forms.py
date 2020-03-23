810356980875e42fa0677b94174db34668cb3bba
Bundit <bundittsp@gmail.com>
2020-03-23 17:25:04 +0700

    Key for WEEK 11 exercise 2 - validators

--- polls/forms.py
+++ polls/forms.py
@@ -3,33 +3,40 @@ from datetime import datetime
 from django import forms
 
 
+def validate_past_date(value):
+    if value < datetime.now().date():
+        raise forms.ValidationError(
+                'วันที่เลือกต้องเป็นวันหลังวันปัจจุบัน'
+            )
+
+
 # Create your forms here
 class PollForm(forms.Form):
     title = forms.CharField(max_length=100, label='หัวข้อ')
-    start_date = forms.DateField(required=False, label='วันเริ่มเปิดโหวต')
-    end_date = forms.DateField(required=False, label='วันสิ้นสุดการโหวต')
+    start_date = forms.DateField(required=False, label='วันเริ่มเปิดโหวต', validators=[validate_past_date])
+    end_date = forms.DateField(required=False, label='วันสิ้นสุดการโหวต', validators=[validate_past_date])
 
     title.widget.attrs.update({'class': 'form-control'})
     start_date.widget.attrs.update({'class': 'form-control'})
     end_date.widget.attrs.update({'class': 'form-control'})
 
-    def clean_start_date(self):
-        data = self.cleaned_data.get('start_date')
+    # def clean_start_date(self):
+    #     data = self.cleaned_data.get('start_date')
         
-        if data < datetime.now().date():
-            raise forms.ValidationError(
-                'วันเริ่มต้นต้องเป็นวันหลังวันปัจจุบัน'
-            )
-        return data
+    #     if data < datetime.now().date():
+    #         raise forms.ValidationError(
+    #             'วันเริ่มต้นต้องเป็นวันหลังวันปัจจุบัน'
+    #         )
+    #     return data
     
-    def clean_end_date(self):
-        data = self.cleaned_data.get('end_date')
+    # def clean_end_date(self):
+    #     data = self.cleaned_data.get('end_date')
         
-        if data < datetime.now().date():
-            raise forms.ValidationError(
-                'วันสิ้นสุดต้องเป็นวันหลังวันปัจจุบัน'
-            )
-        return data
+    #     if data < datetime.now().date():
+    #         raise forms.ValidationError(
+    #             'วันสิ้นสุดต้องเป็นวันหลังวันปัจจุบัน'
+    #         )
+    #     return data
     
     def clean(self):
         cleaned_data = super().clean()
